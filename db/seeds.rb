bdf9856e2c4c145b700f99356f829e2aef3024af
Henri CLOCUH <henriclocuh@yahoo.fr>
2017-09-26 07:04:41 +0200

    Integrated index action for Portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,9 +24,9 @@ puts "5 skills created"
 	Portfolio.create!(
 		title: "Portfolio title: #{portfolio_item}",
 		subtitle:"My great service",
-		body: "typesetting, remaining essentially unchanged. It 
-				was popularised in the 1960s with the release of 
-				Letraset sheets containing Lorem ",
+		body: " typesetting, remaining essentially unchanged. It 
+			   was popularised in the 1960s with the release of 
+			   Letraset sheets containing Lorem ",
 		main_image: "https://place-hold.it/600x400",
 		thumb_image:"https://place-hold.it/350x200",
 		)
